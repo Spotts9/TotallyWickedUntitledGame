ee85f0c3514317d487689cafb96605b570fb182d
emmysyn <emmyspotts@gmail.com>
2020-10-14 04:02:03 +0000

    added player animation

--- src/index.js
+++ src/index.js
@@ -22,5 +22,25 @@ function preload() {
 
 function create() {
     this.player=this.add.sprite(25,300,"dude");
+this.player.setBounce(0.2);
+// this.player.setCollideWorldBounds(false);
+this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers("dude", {start:0, end:3}),
+    frameRate: 10,
+    repeat: -1
+});
 
+this.anims.create({
+    key: 'turn',
+    frames: [{key: "dude", frame: 4}],
+    frameRate: 20
+});
+
+this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers("dude", {start:5, end:8}),
+    frameRate: 10,
+    repeat: -1
+});
 }
